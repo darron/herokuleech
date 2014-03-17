9870da5e4b26e657a25a30eec425db402cc01387
Darron Froese <darron@froese.org>
2014-03-17 15:08:13 -0600

    Adding Sendgrid.

--- config/apps.rb
+++ config/apps.rb
@@ -30,6 +30,15 @@ Padrino.configure_apps do
   set :session_secret, ENV['SESSION_SECRET']
   set :protection, :except => :path_traversal
   set :protect_from_csrf, true
+
+  set :delivery_method, :smtp => {
+    :address         => 'smtp.sendgrid.net',
+    :port            => '587',
+    :user_name       => ENV['SENDGRID_USERNAME'],
+    :password        => ENV['SENDGRID_PASSWORD'],
+    :authentication  => :plain, # :plain, :login, :cram_md5, no auth by default
+    :domain          => "heroku.com" # the HELO domain provided by the client to the server
+  }
 end
 
 # Mounts the core application for this project
