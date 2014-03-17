3c7802b0a1231708cd4b0727ad633b2a346264e6
Darron Froese <darron@froese.org>
2014-03-17 14:49:54 -0600

    Add Sites model.

--- admin/app.rb
+++ admin/app.rb
@@ -34,6 +34,7 @@ module Herokuleech
     end
 
     access_control.roles_for :admin do |role|
+      role.project_module :sites, '/sites'
       role.project_module :accounts, '/accounts'
     end
 
